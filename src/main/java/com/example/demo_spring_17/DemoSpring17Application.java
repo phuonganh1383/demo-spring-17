73fd81d1d3cc3db2db7d11344518b5be6950afda
anhntp <anhntp-ts@hikesiya.co.jp>
2025-11-09 14:06:53 +0700

    init

---
+++ src/main/java/com/example/demo_spring_17/DemoSpring17Application.java
@@ -0,0 +1,13 @@
+package com.example.demo_spring_17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpring17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpring17Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_spring_17/DemoSpring17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_spring_17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpring17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
